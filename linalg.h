7e1eda5f4887e9e17c9c980ffc5bf753eccd26d0
office <hcloaa@connect.ust.hk>
2022-06-30 18:06:47 +0800

    modified linalg.h

--- linalg.h
+++ linalg.h
@@ -6,36 +6,34 @@
 
 template<class num_type>
 class matrix {
-
-public:
+private:
 	int row;
 	int col;
-	std::vector<std::vector<num_type>> mat;
+	num_type** ary;
 
-	// Default constructor
-	matrix() : row(0), col(0), mat({ {} }) {};
-
-	// Copy constructor
-	/*
-	matrix(const matrix& A) {
-		mat = {};
-		std::vector<num_type> col_vec = {};
-		for (int i_row = 0; i_row < A.row; i_row++) {
-			for (int i_col = 0; i_col < A.col; i_col++) {
-				col_vec.push_back(A[i_row][i_col]);
-			}
-			mat.push_back(col_vec);
-			col_vec.clear();
+	num_type** init(int r, int c) {
+		num_type** ary = new num_type * [r];
+		for (int i_r = 0; i_r < r; i_r++) {
+			ary[i_r] = new num_type * [c];
 		}
-	};
-*/
-	// Type conversion constructor
-	matrix(const std::vector<std::vector<num_type>> A) : mat(A), row(A.size()), col(A[0].size()) {};
+		return ary;
+	}
+	
+public:
 
-	std::vector<int> operator[](int n) {
-		return mat[n];
+	/* --------------------------------------------------------- 
+		CONSTRUCTOR
+	   ---------------------------------------------------------
+	*/
+
+	matrix(int r = 3, int c = 3) : row(r), col(c) {
+		ary = init(row, col);
 	}
 
+	/* ---------------------------------------------------------
+		OPERATOR OVERLOAD
+	   ---------------------------------------------------------
+	*/
 	matrix operator+(const matrix& A) {
 		if (this->row != A.row || this->col != A.col) {
 			std::cout << "This dimensions of the two matrices do not match." << std::endl;
@@ -43,12 +41,41 @@ public:
 		matrix result = *this;
 		for (int i_row = 0; i_row < this->row; i_row++) {
 			for (int i_col = 0; i_col < this->col; i_col++) {
-				result.mat[i_row][i_col] = this->mat[i_row][i_col] + A.mat[i_row][i_col];
+				result.mat[i_row][i_col] += A.mat[i_row][i_col];
 			}
 		}
 		return result;
 	}
 	
+	/* ---------------------------------------------------------
+		USEFUL METHODS
+	   ---------------------------------------------------------
+	*/
+	int ROW() { return row; }
+	int COL() { return col; }
+
+	// Print out the matrix
+	void print() {
+		for (int i_row = 0; i_row < row; i_row++) {
+			for (int i_col = 0; i_col < col; i_col++) {
+				if (i_row == 0 && i_col == 0) {
+					std::cout << "[" << ary[i_row][i_col] << ",";
+				}
+				else if (i_col == 0) {
+					std::cout << " " << ary[i_row][i_col] << ",";
+				}
+				else if (i_col < col - 1) {
+					std::cout << ary[i_row][i_col] << ",";
+				}
+				else if (i_col == col - 1 && i_row < row - 1) {
+					std::cout << ary[i_row][i_col] << ";" << std::endl;
+				}
+				else {
+					std::cout << ary[i_row][i_col] << "]" << std::endl;
+				}
+			}
+		}
+	}
 };
 
 #endif // ! LINALG_H
